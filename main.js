3e17345e13cd7e8c6767a4ccafeb539687bd5ca7
limziyun <39241304+limziyun@users.noreply.github.com>
2018-10-27 01:34:31 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,36 @@
+var db = firebase
+  .initializeApp({
+    databaseURL: "https://space-czar.firebaseio.com/"
+  })
+  .database();
+
+var realtimeRef = db.ref("realtime");
+var forecastRef = db.ref("forecast");
+
 new Vue({
-  render: h => h(App)
-}).$mount(`#app`);
+  el: "#app",
+  data: {
+    counter: 0,
+    location: "",
+    occupant: {}
+    //databaseURL:
+  },
+  //firebase: {},
+  methods: {
+    exit: function() {
+      console.log(forecastRef.child());
+      forecastRef.child(forecast[location]).push({
+        Data: this.occupant // mot sure if correct
+      });
+      // remove from realtime
+      realtimeRef
+        .child(realtime[location])
+        .child("occupants")
+        .child(occupant)
+        .remove();
+    },
+    increment: function() {
+      this.counter++;
+    }
+  }
+});
